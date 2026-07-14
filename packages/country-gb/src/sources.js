1d48825420dda560eb927a48516a3e7f0ee7ee45
thedanieltan <danieltanyl91@gmail.com>
2026-07-14 09:11:56 +0800

    WP-12: add HMRC official source

---
+++ packages/country-gb/src/sources.js
@@ -0,0 +1,10 @@
+export const UK_SOURCES = Object.freeze([
+  {
+    sourceId: "gb-hmrc-income-tax-rates-2024-2027",
+    publisher: "HM Revenue & Customs",
+    publisherType: "tax-authority",
+    title: "Income Tax rates and allowances for current and previous tax years",
+    url: "https://www.gov.uk/government/publications/rates-and-allowances-income-tax/income-tax-rates-and-allowances-current-and-past",
+    retrievedAt: "2026-07-14"
+  }
+]);
