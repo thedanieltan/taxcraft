d32014b46efa827c32b2bc9b150c21efe7d34879
thedanieltan <danieltanyl91@gmail.com>
2026-07-18 21:42:23 +0800

    feat(api): register household-filing country packages

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -8,6 +8,7 @@ import { createTaxCraft } from "@taxcraft/core";
 import { complexCompositePackages } from "@taxcraft/country-complex-composite";
 import { flatRatePackages } from "@taxcraft/country-flat-rate";
 import { ukPackage } from "@taxcraft/country-gb";
+import { householdFilingPackages } from "@taxcraft/country-household-filing";
 import { noPitPackages } from "@taxcraft/country-no-pit";
 import { progressiveReliefPackages } from "@taxcraft/country-progressive-reliefs";
 import { calculateChargeableIncomeWorksheet, singaporePackage } from "@taxcraft/country-sg";
@@ -21,6 +22,7 @@ const countryPackages = Object.freeze([
   ...simpleProgressivePackages,
   ...progressiveReliefPackages,
   ...complexCompositePackages,
+  ...householdFilingPackages,
 ]);
 const packagesByJurisdiction = new Map(countryPackages.map((countryPackage) => [countryPackage.manifest.jurisdiction, countryPackage]));
 const engine = createTaxCraft({ countryPackages });
