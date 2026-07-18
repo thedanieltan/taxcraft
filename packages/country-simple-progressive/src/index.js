dfdc4ba96bc311499653ac94f7d9df0cc8cd48f0
thedanieltan <danieltanyl91@gmail.com>
2026-07-18 20:19:33 +0800

    feat(pit): compose sixth simple-progressive wave

--- packages/country-simple-progressive/src/index.js
+++ packages/country-simple-progressive/src/index.js
@@ -14,12 +14,17 @@ import {
   SIMPLE_PROGRESSIVE_WAVE_5_JURISDICTIONS,
   simpleProgressiveWave5Packages,
 } from "./wave5.js";
+import {
+  SIMPLE_PROGRESSIVE_WAVE_6_JURISDICTIONS,
+  simpleProgressiveWave6Packages,
+} from "./wave6.js";
 
 export const SIMPLE_PROGRESSIVE_JURISDICTIONS = Object.freeze([
   ...acceptedDefinitions,
   ...SIMPLE_PROGRESSIVE_WAVE_3_JURISDICTIONS,
   ...SIMPLE_PROGRESSIVE_WAVE_4_JURISDICTIONS,
   ...SIMPLE_PROGRESSIVE_WAVE_5_JURISDICTIONS,
+  ...SIMPLE_PROGRESSIVE_WAVE_6_JURISDICTIONS,
 ]);
 
 export const simpleProgressivePackages = Object.freeze([
@@ -27,6 +32,7 @@ export const simpleProgressivePackages = Object.freeze([
   ...simpleProgressiveWave3Packages,
   ...simpleProgressiveWave4Packages,
   ...simpleProgressiveWave5Packages,
+  ...simpleProgressiveWave6Packages,
 ]);
 
 export const simpleProgressivePackagesByJurisdiction = Object.freeze(Object.fromEntries(
