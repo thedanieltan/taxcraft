4c721422443d884c3b6c6dedda32ef3fa95ebb17
thedanieltan <danieltanyl91@gmail.com>
2026-07-18 20:34:57 +0800

    feat(pit): compose Malaysia progressive-reliefs package

--- packages/country-progressive-reliefs/src/index.js
+++ packages/country-progressive-reliefs/src/index.js
@@ -3,15 +3,18 @@ import {
   progressiveReliefPackagesByJurisdiction as acceptedPackagesByJurisdiction,
 } from "./base.js";
 import { southAfricaPackage } from "./south-africa.js";
+import { malaysiaPackage } from "./malaysia.js";
 
 export const progressiveReliefPackages = Object.freeze([
   ...acceptedPackages,
   southAfricaPackage,
+  malaysiaPackage,
 ]);
 
 export const progressiveReliefPackagesByJurisdiction = Object.freeze({
   ...acceptedPackagesByJurisdiction,
   ZA: southAfricaPackage,
+  MY: malaysiaPackage,
 });
 
-export { southAfricaPackage };
+export { southAfricaPackage, malaysiaPackage };
