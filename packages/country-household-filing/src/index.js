251ecac354a4ea212e33c87ef4f7ece560311904
thedanieltan <danieltanyl91@gmail.com>
2026-07-21 12:15:12 +0800

    Register Malta household-filing package

--- packages/country-household-filing/src/index.js
+++ packages/country-household-filing/src/index.js
@@ -1,14 +1,17 @@
 import { irelandPackage } from "./base.js";
 import { polandPackage } from "./poland.js";
+import { maltaPackage } from "./malta.js";
 
-export { irelandPackage, polandPackage };
+export { irelandPackage, polandPackage, maltaPackage };
 
 export const householdFilingPackages = Object.freeze([
   irelandPackage,
   polandPackage,
+  maltaPackage,
 ]);
 
 export const householdFilingPackagesByJurisdiction = Object.freeze({
   IE: irelandPackage,
   PL: polandPackage,
+  MT: maltaPackage,
 });
