22f31ad378001b2ea807b6729373244edef2b4a2
thedanieltan <danieltanyl91@gmail.com>
2026-07-22 15:57:29 +0800

    Register Isle of Man in household-filing bundle guard

--- test/household-filing-packages.test.js
+++ test/household-filing-packages.test.js
@@ -8,12 +8,13 @@ import {
 test("household-filing bundle retains every maintained package", () => {
   assert.deepEqual(
     householdFilingPackages.map(({ manifest }) => manifest.jurisdiction),
-    ["IE", "PL", "MT", "PT", "DE"],
+    ["IE", "PL", "MT", "PT", "DE", "IM"],
   );
   assert.equal(householdFilingPackagesByJurisdiction.IE.manifest.jurisdiction, "IE");
   assert.equal(householdFilingPackagesByJurisdiction.PL.manifest.jurisdiction, "PL");
   assert.equal(householdFilingPackagesByJurisdiction.MT.manifest.jurisdiction, "MT");
   assert.equal(householdFilingPackagesByJurisdiction.PT.manifest.jurisdiction, "PT");
   assert.equal(householdFilingPackagesByJurisdiction.DE.manifest.jurisdiction, "DE");
+  assert.equal(householdFilingPackagesByJurisdiction.IM.manifest.jurisdiction, "IM");
   assert.ok(householdFilingPackages.every(({ manifest }) => manifest.storesUserPII === false));
 });
