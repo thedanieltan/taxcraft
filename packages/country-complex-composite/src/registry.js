4c84d984cb7beefe4f2c5f4dabf80feb4b58406e
thedanieltan <danieltanyl91@gmail.com>
2026-07-25 23:11:41 +0800

    WP-PIT-14: register priority-market composite packages

--- packages/country-complex-composite/src/registry.js
+++ packages/country-complex-composite/src/registry.js
@@ -3,21 +3,43 @@ import {
   unitedStatesPackage,
 } from "./index.js";
 import { norwayPackage } from "./norway.js";
+import {
+  indiaPackage,
+  canadaPackage,
+  japanPackage,
+  spainPackage,
+  italyPackage,
+} from "./priority-markets.js";
 
 export const complexCompositePackages = Object.freeze([
   hongKongPackage,
   unitedStatesPackage,
   norwayPackage,
+  indiaPackage,
+  canadaPackage,
+  japanPackage,
+  spainPackage,
+  italyPackage,
 ]);
 
 export const complexCompositePackagesByJurisdiction = Object.freeze({
   HK: hongKongPackage,
   US: unitedStatesPackage,
   NO: norwayPackage,
+  IN: indiaPackage,
+  CA: canadaPackage,
+  JP: japanPackage,
+  ES: spainPackage,
+  IT: italyPackage,
 });
 
 export {
   hongKongPackage,
   unitedStatesPackage,
   norwayPackage,
+  indiaPackage,
+  canadaPackage,
+  japanPackage,
+  spainPackage,
+  italyPackage,
 };
