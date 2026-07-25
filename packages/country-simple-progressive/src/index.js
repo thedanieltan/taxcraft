fc5f3f05234dff0ea9f64fcb42605022e721b046
thedanieltan <danieltanyl91@gmail.com>
2026-07-25 22:50:52 +0800

    WP-PIT-14: register simple progressive wave 15

--- packages/country-simple-progressive/src/index.js
+++ packages/country-simple-progressive/src/index.js
@@ -50,6 +50,10 @@ import {
   SIMPLE_PROGRESSIVE_WAVE_14_JURISDICTIONS,
   simpleProgressiveWave14Packages,
 } from "./wave14.js";
+import {
+  SIMPLE_PROGRESSIVE_WAVE_15_JURISDICTIONS,
+  simpleProgressiveWave15Packages,
+} from "./wave15.js";
 
 export const SIMPLE_PROGRESSIVE_JURISDICTIONS = Object.freeze([
   ...acceptedDefinitions,
@@ -65,6 +69,7 @@ export const SIMPLE_PROGRESSIVE_JURISDICTIONS = Object.freeze([
   ...SIMPLE_PROGRESSIVE_WAVE_12_JURISDICTIONS,
   ...SIMPLE_PROGRESSIVE_WAVE_13_JURISDICTIONS,
   ...SIMPLE_PROGRESSIVE_WAVE_14_JURISDICTIONS,
+  ...SIMPLE_PROGRESSIVE_WAVE_15_JURISDICTIONS,
 ]);
 
 export const simpleProgressivePackages = Object.freeze([
@@ -81,6 +86,7 @@ export const simpleProgressivePackages = Object.freeze([
   ...simpleProgressiveWave12Packages,
   ...simpleProgressiveWave13Packages,
   ...simpleProgressiveWave14Packages,
+  ...simpleProgressiveWave15Packages,
 ]);
 
 export const simpleProgressivePackagesByJurisdiction = Object.freeze(Object.fromEntries(
