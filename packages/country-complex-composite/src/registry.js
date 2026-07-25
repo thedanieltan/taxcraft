9f7e6230dd9fa4895a6ffbebc02df40aa4583895
thedanieltan <danieltanyl91@gmail.com>
2026-07-25 23:32:15 +0800

    WP-PIT-14: use corrected priority-market models

--- packages/country-complex-composite/src/registry.js
+++ packages/country-complex-composite/src/registry.js
@@ -9,7 +9,7 @@ import {
   japanPackage,
   spainPackage,
   italyPackage,
-} from "./priority-markets-v2.js";
+} from "./priority-markets-v3.js";
 
 export const complexCompositePackages = Object.freeze([
   hongKongPackage,
