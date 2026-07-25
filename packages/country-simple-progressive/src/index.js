fe863f286c9a0a2f35870cb2d2b1e14d2248199a
thedanieltan <danieltanyl91@gmail.com>
2026-07-25 23:00:12 +0800

    WP-PIT-14: preserve legacy bundle and export wave 15 separately

--- packages/country-simple-progressive/src/index.js
+++ packages/country-simple-progressive/src/index.js
@@ -50,7 +50,8 @@ import {
   SIMPLE_PROGRESSIVE_WAVE_14_JURISDICTIONS,
   simpleProgressiveWave14Packages,
 } from "./wave14.js";
-import {
+
+export {
   SIMPLE_PROGRESSIVE_WAVE_15_JURISDICTIONS,
   simpleProgressiveWave15Packages,
 } from "./wave15.js";
@@ -69,7 +70,6 @@ export const SIMPLE_PROGRESSIVE_JURISDICTIONS = Object.freeze([
   ...SIMPLE_PROGRESSIVE_WAVE_12_JURISDICTIONS,
   ...SIMPLE_PROGRESSIVE_WAVE_13_JURISDICTIONS,
   ...SIMPLE_PROGRESSIVE_WAVE_14_JURISDICTIONS,
-  ...SIMPLE_PROGRESSIVE_WAVE_15_JURISDICTIONS,
 ]);
 
 export const simpleProgressivePackages = Object.freeze([
@@ -86,7 +86,6 @@ export const simpleProgressivePackages = Object.freeze([
   ...simpleProgressiveWave12Packages,
   ...simpleProgressiveWave13Packages,
   ...simpleProgressiveWave14Packages,
-  ...simpleProgressiveWave15Packages,
 ]);
 
 export const simpleProgressivePackagesByJurisdiction = Object.freeze(Object.fromEntries(
